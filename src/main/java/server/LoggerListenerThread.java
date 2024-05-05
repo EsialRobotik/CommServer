ac4a3df57104e760604e50b385450260ed2eeef7
FuseTim <fusetim@gmx.com>
2024-05-05 12:21:09 +0200

    feat: log every robots as a separated file
    Also made sure the "deprecated" gradle 4.10 wrapper works!

--- src/main/java/server/LoggerListenerThread.java
+++ src/main/java/server/LoggerListenerThread.java
@@ -3,7 +3,9 @@ package server;
 import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.text.SimpleDateFormat;
 
 public class LoggerListenerThread extends Thread {
 
@@ -13,10 +15,13 @@ public class LoggerListenerThread extends Thread {
     protected BufferedReader reader;
     protected PrintWriter writer;
 
+    private PrintWriter fileWriter;
+
     public LoggerListenerThread(Socket clientSocket, SimpleServer simpleServer) throws IOException {
         this.socket = clientSocket;
         this.simpleServer = simpleServer;
         this.createReaderAndWriter();
+        this.createFileWriter();
     }
 
     protected void createReaderAndWriter() throws IOException {
@@ -25,6 +30,17 @@ public class LoggerListenerThread extends Thread {
 
         OutputStream output = this.socket.getOutputStream();
         this.writer = new PrintWriter(output, true);
+    }
+
+    private void createFileWriter() throws IOException {
+        File logDir = new File("./logs");
+        if (!logDir.exists()) logDir.mkdir();
+        // Create the file named `<date>-<client_ip>-log.txt`
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        String date = simpleDateFormat.format(new Date());
+        String clientIp = this.socket.getInetAddress().getHostAddress();
+        String fileName = "logs/" + date + "_" + clientIp + "_log.txt";
+        this.fileWriter = new PrintWriter(new FileWriter(fileName, false));
     }
 
     public void run() {
@@ -40,6 +56,10 @@ public class LoggerListenerThread extends Thread {
                     continue;
                 } else {
                     System.out.println("server.LoggerThread - " + line);
+                    if (fileWriter != null) {
+                        fileWriter.println(line);
+                        fileWriter.flush();
+                    }
                     this.simpleServer.broadcastLogMessage(line);
                 }
             }
